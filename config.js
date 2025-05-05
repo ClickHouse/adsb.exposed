5a11b58c70f7f1a7ccc89712c0a37ae4101e056e
Alexey Milovidov <milovidov@clickhouse.com>
2025-05-06 00:07:33 +0200

    Generalization

--- config.js
+++ config.js
@@ -36,9 +36,37 @@ const datasets = {
             { table: 'planes_mercator_sample10',  sample: 10,  priority: 2 },
             { table: 'planes_mercator',           sample: 1,   priority: 3 },
         ],
+        report_total: {
+            query: (condition => `
+                WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
+                    AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
+                SELECT
+                    count() AS traces,
+                    uniq(r) AS aircrafts,
+                    uniq(t) AS types,
+                    uniqIf(aircraft_flight,
+                    aircraft_flight != '') AS flights,
+                    min(time) AS first, max(time) AS last
+                FROM {table:Identifier}
+                WHERE ${condition}`),
+            content: (json => {
+                let row = json.data[0];
+                let text = `Total ${Number(row.traces).toLocaleString()} traces, ${Number(row.aircrafts).toLocaleString()} aircrafts of ${Number(row.types).toLocaleString()} types, ${Number(row.aircrafts).toLocaleString()} flight nums.`;
+
+                if (row.traces > 0) {
+                    text += ` Time: ${row.first} — ${row.last}.`;
+                }
+
+                if (json.statistics.rows_read > 1) {
+                    text += ` Processed ${Number(json.statistics.rows_read).toLocaleString()} rows.`;
+                }
+
+                return text;
+            }),
+        },
         reports: [
             {
-                query: `
+                query: (condition => `
                     WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
                         AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
                     SELECT aircraft_flight, count() AS c
@@ -46,7 +74,7 @@ const datasets = {
                     WHERE aircraft_flight != '' AND NOT startsWith(aircraft_flight, '@@@') AND ${condition}
                     GROUP BY aircraft_flight
                     ORDER BY c DESC
-                    LIMIT 100`,
+                    LIMIT 100`),
                 field: 'aircraft_flight',
                 id: 'report_flights',
                 title: 'Flights: ',
@@ -54,7 +82,7 @@ const datasets = {
                 content: (row => row.aircraft_flight)
             },
             {
-                query: `
+                query: (condition => `
                     WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
                         AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
                     SELECT t, anyIf(desc, desc != '') AS desc, count() AS c
@@ -62,7 +90,7 @@ const datasets = {
                     WHERE t != '' AND ${condition}
                     GROUP BY t
                     ORDER BY c DESC
-                    LIMIT 100`,
+                    LIMIT 100`),
                 field: 't',
                 id: 'report_types',
                 title: 'Types:\n',
@@ -70,7 +98,7 @@ const datasets = {
                 content: (row => `${row.t} (${row.desc})`)
             },
             {
-                query: `
+                query: (condition => `
                     WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
                         AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
                     SELECT r, count() AS c
@@ -78,7 +106,7 @@ const datasets = {
                     WHERE r != '' AND ${condition}
                     GROUP BY r
                     ORDER BY c DESC
-                    LIMIT 100`,
+                    LIMIT 100`),
                 field: 'r',
                 id: 'report_regs',
                 title: 'Registration: ',
@@ -86,7 +114,7 @@ const datasets = {
                 content: (row => row.r)
             },
             {
-                query: `
+                query: (condition => `
                     WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
                         AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
                     SELECT ownOp, count() AS c
@@ -94,7 +122,7 @@ const datasets = {
                     WHERE ownOp != '' AND ${condition}
                     GROUP BY ownOp
                     ORDER BY c DESC
-                    LIMIT 100`,
+                    LIMIT 100`),
                 field: 'ownOp',
                 id: 'report_owners',
                 title: 'Owner:\n',
@@ -926,5 +954,147 @@ FROM {table:Identifier}
 WHERE in_tile AND aircraft_flight != ''
 GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
         }
-    }
+    },
+
+    "Places": {
+        endpoints: [
+            {
+                name: "Cloud (Real-Time)",
+                urls: [
+                    {
+                        url: "https://kvzqttvc2n.eu-west-1.aws.clickhouse-staging.com",
+                        sticky: "https://{hash}.sticky.kvzqttvc2n.eu-west-1.aws.clickhouse-staging.com",
+                    }
+                ]
+            },
+        ],
+        levels: [
+            { table: 'foursquare_mercator', sample: 1, priority: 1 },
+        ],
+        report_total: {
+            query: (condition => `
+                WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
+                    AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
+                SELECT
+                    count() AS places
+                FROM {table:Identifier}
+                WHERE ${condition}`),
+            content: (json => {
+                let row = json.data[0];
+                let text = `Total ${Number(row.places).toLocaleString()} places.`;
+
+                if (json.statistics.rows_read > 1) {
+                    text += ` Processed ${Number(json.statistics.rows_read).toLocaleString()} rows.`;
+                }
+
+                return text;
+            }),
+        },
+        reports: [
+        ],
+        queries: {
+            "Density": `WITH
+    bitShiftLeft(1::UInt64, {z:UInt8}) AS zoom_factor,
+    bitShiftLeft(1::UInt64, 32 - {z:UInt8}) AS tile_size,
+
+    tile_size * {x:UInt16} AS tile_x_begin,
+    tile_size * ({x:UInt16} + 1) AS tile_x_end,
+
+    tile_size * {y:UInt16} AS tile_y_begin,
+    tile_size * ({y:UInt16} + 1) AS tile_y_end,
+
+    mercator_x >= tile_x_begin AND mercator_x < tile_x_end
+    AND mercator_y >= tile_y_begin AND mercator_y < tile_y_end AS in_tile,
+
+    bitShiftRight(mercator_x - tile_x_begin, 32 - 10 - {z:UInt8}) AS x,
+    bitShiftRight(mercator_y - tile_y_begin, 32 - 10 - {z:UInt8}) AS y,
+
+    y * 1024 + x AS pos,
+
+    count() AS total,
+
+    pow(least(1, total / 100 * zoom_factor), 1/5) AS color1,
+    pow(least(1, total / 10000 * zoom_factor), 1/5) AS color2,
+    pow(least(1, total / 1000000 * zoom_factor), 1/5) AS color3,
+
+    255 AS alpha,
+    color1 * 255 AS red,
+    color2 * 255 AS green,
+    color3 * 255 AS blue
+
+    SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
+    FROM {table:Identifier}
+    WHERE in_tile
+    GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`,
+
+            "Old vs New": `WITH
+    bitShiftLeft(1::UInt64, {z:UInt8}) AS zoom_factor,
+    bitShiftLeft(1::UInt64, 32 - {z:UInt8}) AS tile_size,
+
+    tile_size * {x:UInt16} AS tile_x_begin,
+    tile_size * ({x:UInt16} + 1) AS tile_x_end,
+
+    tile_size * {y:UInt16} AS tile_y_begin,
+    tile_size * ({y:UInt16} + 1) AS tile_y_end,
+
+    mercator_x >= tile_x_begin AND mercator_x < tile_x_end
+    AND mercator_y >= tile_y_begin AND mercator_y < tile_y_end AS in_tile,
+
+    bitShiftRight(mercator_x - tile_x_begin, 32 - 10 - {z:UInt8}) AS x,
+    bitShiftRight(mercator_y - tile_y_begin, 32 - 10 - {z:UInt8}) AS y,
+
+    y * 1024 + x AS pos,
+
+    count() AS total,
+
+    greatest(0, avg(date_created::Int32 - '2009-01-01'::Date::Int32) / (today()::Int32 - '2009-01-01'::Date::Int32)) AS color1,
+    pow(least(1, total / 10000 * zoom_factor), 1/5) AS color2,
+
+    255 AS alpha,
+    color1 * 255 AS red,
+    color2 * 255 AS green,
+    color2 * 255 AS blue
+
+    SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
+    FROM {table:Identifier}
+    WHERE in_tile AND date_created IS NOT NULL
+    GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`,
+
+            "Countries": `WITH
+    bitShiftLeft(1::UInt64, {z:UInt8}) AS zoom_factor,
+    bitShiftLeft(1::UInt64, 32 - {z:UInt8}) AS tile_size,
+
+    tile_size * {x:UInt16} AS tile_x_begin,
+    tile_size * ({x:UInt16} + 1) AS tile_x_end,
+
+    tile_size * {y:UInt16} AS tile_y_begin,
+    tile_size * ({y:UInt16} + 1) AS tile_y_end,
+
+    mercator_x >= tile_x_begin AND mercator_x < tile_x_end
+    AND mercator_y >= tile_y_begin AND mercator_y < tile_y_end AS in_tile,
+
+    bitShiftRight(mercator_x - tile_x_begin, 32 - 10 - {z:UInt8}) AS x,
+    bitShiftRight(mercator_y - tile_y_begin, 32 - 10 - {z:UInt8}) AS y,
+
+    y * 1024 + x AS pos,
+
+    count() AS total,
+
+    cityHash64(country) MOD 256 AS color1,
+    cityHash64(country) DIV 256 MOD 256 AS color2,
+    cityHash64(country) DIV 65536 MOD 256 AS color3,
+
+    pow(least(1, total / 1000 * zoom_factor), 1/5) AS transparency,
+
+    transparency * 255 AS alpha,
+    avg(color1) AS red,
+    avg(color2) AS green,
+    avg(color3) AS blue
+
+    SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
+    FROM {table:Identifier}
+    WHERE in_tile
+    GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
+        },
+    },
 };
