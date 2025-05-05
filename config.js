119bca4c4888516a8da82118b3a3e117c9811415
Alexey Milovidov <milovidov@clickhouse.com>
2025-05-06 01:26:16 +0200

    Generalization; more examples; fix SQL injection

--- config.js
+++ config.js
@@ -993,6 +993,38 @@ GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
             }),
         },
         reports: [
+            {
+                query: (condition => `
+                    WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
+                        AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
+                    SELECT name, count() AS c
+                    FROM {table:Identifier}
+                    WHERE name != '' AND ${condition}
+                    GROUP BY name
+                    ORDER BY c DESC
+                    LIMIT 100`),
+                field: 'name',
+                id: 'report_names',
+                title: 'Places: ',
+                separator: ', ',
+                content: (row => `${row.name}${row.c > 1 ? ` (${row.c})` : ''}`)
+            },
+            {
+                query: (condition => `
+                    WITH mercator_x >= {left:UInt32} AND mercator_x < {right:UInt32}
+                        AND mercator_y >= {top:UInt32} AND mercator_y < {bottom:UInt32} AS in_tile
+                    SELECT category, count() AS c
+                    FROM {table:Identifier}
+                    WHERE category != '' AND ${condition}
+                    GROUP BY category
+                    ORDER BY c DESC
+                    LIMIT 25`),
+                field: 'category',
+                id: 'report_categories',
+                title: 'Categories: ',
+                separator: '\n',
+                content: (row => `${row.category} (${row.c})`)
+            },
         ],
         queries: {
             "Density": `WITH
@@ -1096,6 +1128,73 @@ GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
     SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
     FROM {table:Identifier}
     WHERE in_tile
+    GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`,
+
+            "Coffeeshops": `WITH
+    bitShiftLeft(1::UInt64, {z:UInt8}) AS zoom_factor,
+    bitShiftLeft(1::UInt64, 32 - {z:UInt8}) AS tile_size,
+
+    tile_size * {x:UInt16} AS tile_x_begin,
+    tile_size * ({x:UInt16} + 1) AS tile_x_end,
+
+    tile_size * {y:UInt16} AS tile_y_begin,
+    tile_size * ({y:UInt16} + 1) AS tile_y_end,
+
+    mercator_x >= tile_x_begin AND mercator_x < tile_x_end
+    AND mercator_y >= tile_y_begin AND mercator_y < tile_y_end AS in_tile,
+
+    bitShiftRight(mercator_x - tile_x_begin, 32 - 10 - {z:UInt8}) AS x,
+    bitShiftRight(mercator_y - tile_y_begin, 32 - 10 - {z:UInt8}) AS y,
+
+    y * 1024 + x AS pos,
+
+    count() AS total,
+
+    pow(least(1, total / 100 * zoom_factor), 1/5) AS color1,
+    pow(least(1, total / 10000 * zoom_factor), 1/5) AS color2,
+    pow(least(1, total / 1000000 * zoom_factor), 1/5) AS color3,
+
+    255 AS alpha,
+    color1 * 255 AS red,
+    color2 * 255 AS green,
+    color3 * 255 AS blue
+
+    SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
+    FROM {table:Identifier}
+    WHERE in_tile AND category = 'Retail > Marijuana Dispensary'
+    GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`,
+
+            "Casinos": `WITH
+    bitShiftLeft(1::UInt64, {z:UInt8}) AS zoom_factor,
+    bitShiftLeft(1::UInt64, 32 - {z:UInt8}) AS tile_size,
+
+    tile_size * {x:UInt16} AS tile_x_begin,
+    tile_size * ({x:UInt16} + 1) AS tile_x_end,
+
+    tile_size * {y:UInt16} AS tile_y_begin,
+    tile_size * ({y:UInt16} + 1) AS tile_y_end,
+
+    mercator_x >= tile_x_begin AND mercator_x < tile_x_end
+    AND mercator_y >= tile_y_begin AND mercator_y < tile_y_end AS in_tile,
+
+    bitShiftRight(mercator_x - tile_x_begin, 32 - 10 - {z:UInt8}) AS x,
+    bitShiftRight(mercator_y - tile_y_begin, 32 - 10 - {z:UInt8}) AS y,
+
+    y * 1024 + x AS pos,
+
+    count() AS total,
+
+    greatest(0, avg(date_created::Int32 - '2009-01-01'::Date::Int32) / (today()::Int32 - '2009-01-01'::Date::Int32)) AS color1,
+    pow(least(1, total / 10000 * zoom_factor), 1/5) AS color2,
+
+    255 AS alpha,
+    color1 * 255 AS red,
+    color2 * 255 AS green,
+    color2 * 255 AS blue
+
+    SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
+    FROM {table:Identifier}
+    WHERE in_tile AND date_created IS NOT NULL AND category LIKE '%Casino%'
     GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
         },
     },
