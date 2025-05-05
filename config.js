7327fa11f68d6d45cdf7e3166513e6016e091d5c
Alexey Milovidov <milovidov@clickhouse.com>
2025-05-06 01:41:39 +0200

    Add boats

--- config.js
+++ config.js
@@ -1195,6 +1195,40 @@ GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
     SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
     FROM {table:Identifier}
     WHERE in_tile AND date_created IS NOT NULL AND category LIKE '%Casino%'
+    GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`,
+
+            "Boats": `WITH
+    bitShiftLeft(1::UInt64, {z:UInt8}) AS zoom_factor,
+    bitShiftLeft(1::UInt64, 32 - {z:UInt8}) AS tile_size,
+
+    tile_size * {x:UInt16} AS tile_x_begin,
+    tile_size * ({x:UInt16} + 1) AS tile_x_end,
+
+    tile_size * {y:UInt16} AS tile_y_begin,
+    tile_size * ({y:UInt16} + 1) AS tile_y_end,
+
+    mercator_x >= tile_x_begin AND mercator_x < tile_x_end
+    AND mercator_y >= tile_y_begin AND mercator_y < tile_y_end AS in_tile,
+
+    bitShiftRight(mercator_x - tile_x_begin, 32 - 10 - {z:UInt8}) AS x,
+    bitShiftRight(mercator_y - tile_y_begin, 32 - 10 - {z:UInt8}) AS y,
+
+    y * 1024 + x AS pos,
+
+    count() AS total,
+
+    pow(least(1, total / 100 * zoom_factor), 1/5) AS color1,
+    pow(least(1, total / 10000 * zoom_factor), 1/5) AS color2,
+    pow(least(1, total / 1000000 * zoom_factor), 1/5) AS color3,
+
+    255 AS alpha,
+    color1 * 255 AS red,
+    color2 * 255 AS green,
+    color3 * 255 AS blue
+
+    SELECT round(red)::UInt8, round(green)::UInt8, round(blue)::UInt8, round(alpha)::UInt8
+    FROM {table:Identifier}
+    WHERE in_tile AND date_created IS NOT NULL AND category = 'Travel and Transportation > Boat or Ferry'
     GROUP BY pos ORDER BY pos WITH FILL FROM 0 TO 1024*1024`
         },
     },
